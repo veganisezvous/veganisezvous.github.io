29c0c5ca09166ba51226e093914dae390a1a677a
veganisezvous <veganisezvous@gmail.com>
2020-05-12 23:56:04 +0200

    commit

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,7 @@
 $(document).ready(function () {
 
+	paddingTopWrapper();
+
 	$('.burger').on('click', function() {
 		$('.nav__wrapper').addClass('--is-active');
 	});
@@ -8,6 +10,20 @@ $(document).ready(function () {
 		$('.nav__wrapper').removeClass('--is-active');
 	});
 
+
 });
 
+function paddingTopWrapper() {
 
+	const body = $('body#index');
+	const wrapper = $('.main');
+	const header = $('header').height();
+
+	if (body.length) {
+		wrapper.css('padding-top' , header + 'px');
+	}
+	else {
+		wrapper.css('padding-top' , 0);
+	}
+
+}
